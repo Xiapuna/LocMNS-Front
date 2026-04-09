6edc69eedcd8d17602b4ca238506d5cace32c9f9
Célia <celia.godfrin@gmail.com>
2026-04-09 10:47:21 +0200

    FEAT : Add Home and NotFound pages with basic routing

--- LocMNS-Front/src/app/app.routes.ts
+++ LocMNS-Front/src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
+import { NotFound } from '../pages/not-found/not-found';
+import { Home } from '../pages/home/home';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  // Routgae vers la page "Home"
+  {
+    path: '',
+    component: Home,
+    title: 'LocMNS - Accueil',
+  },
+  // Routgae vers la page "Erreur 404"
+  {
+    path: '**',
+    component: NotFound,
+    title: '404 - Not Found',
+  },
+];
